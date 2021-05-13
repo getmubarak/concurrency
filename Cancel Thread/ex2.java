2bc002dc826ed6314939c26565fcbfadf7801382
getmubarak <getmub@gmail.com>
2021-05-13 14:08:55 +0530

    Create ex2.java

---
+++ Cancel Thread/ex2.java
@@ -0,0 +1,25 @@
+import static java.lang.Thread.currentThread;
+public class ThreadStopTest {
+   public static void main(String args[]) throws InterruptedException {
+      UserThread userThread = new UserThread();
+      Thread thread = new Thread(userThread, "T1");
+      thread.start();
+      Thread.sleep(2000);
+      System.out.println(currentThread().getName() + " is stopping user thread");
+      userThread.stop();
+      thread.join();
+      System.out.println(currentThread().getName() + " is finished now");
+   }
+}
+class UserThread implements Runnable {
+   private boolean exit = false;
+   public void run() {
+      while(!exit) {
+         System.out.println("The user thread is running");
+      }
+      System.out.println("The user thread is now stopped");
+   }
+   public void stop() {
+      exit = true;
+   }
+}
