14565a9565a3a8d6f5a39c0579f6e5f19e1c8514
getmubarak <getmub@gmail.com>
2021-05-13 16:14:25 +0530

    Create ex2.java

---
+++ Sync/ex2.java
@@ -0,0 +1,19 @@
+class Foo
+{
+  int _answer;
+  bool _complete;
+  
+  void A()
+  {
+    _answer = 123;
+    _complete = true;
+  }
+  
+  void B()
+  {
+    if(_complete)
+    {
+      Console.WriteLine (_ answer);
+    }
+  }
+}
