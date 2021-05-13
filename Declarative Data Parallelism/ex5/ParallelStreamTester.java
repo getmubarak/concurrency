dbe03970f9b8a9b8bc9a8531faadfe4b6c6a80c4
getmubarak <getmub@gmail.com>
2021-05-13 15:27:35 +0530

    Update ParallelStreamTester.java

--- Declarative Data Parallelism/ex5/ParallelStreamTester.java
+++ Declarative Data Parallelism/ex5/ParallelStreamTester.java
@@ -5,6 +5,49 @@ import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 
+enum Gender {
+
+	MALE, FEMALE, OTHER
+}
+
+class Person {
+    public Person(String name, int age, String state) {
+		this.name = name;
+		this.age = age;
+		this.state = state;
+		this.gender = Gender.FEMALE;
+	}
+    
+	public Person(String name, int age, Gender gender) {
+		this.name = name;
+		this.age = age;
+		this.state = "NY";
+		this.gender = gender;
+	}
+
+	private final String name;
+    private final int age;
+    private final String state;
+    private final Gender gender;
+    
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getState() {
+		return state;
+	}
+	@Override
+	public String toString() {
+		return "\nPerson [name=" + name + ", age=" + age + ", state=" + state + "]";
+	}
+	public Gender getGender() {
+		return gender;
+	}
+}
+
 public class ParallelStreamTester {
 	static int COLLECTION_SIZE = 100000	;
 	
