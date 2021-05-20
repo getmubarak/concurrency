577f041afd9127ea500c56e967da274c7f3509b7
getmubarak <getmub@gmail.com>
2021-05-20 19:31:51 +0530

    Update concurrent bag.java

--- Sync/concurrent bag.java
+++ Sync/concurrent bag.java
@@ -1,1 +1,14 @@
 
+
+import com.google.common.collect;
+
+public class Main
+{
+ public static void main(String[] args)
+ {
+	 ConcurrentHashMultiset<String> cms = ConcurrentHashMultiset.create();
+	 cms.add("a", 2);
+	 cms.add("b", 3);
+
+ }
+}
